e3f2928a5e7bd0853a0870dbd806fb3aa8fb7334
yotti5160 <40465007+yotti5160@users.noreply.github.com>
2018-10-21 12:48:43 +0800

    Update MinTrailingZeros.py

--- Challenges/MinTrailingZeros.py
+++ Challenges/MinTrailingZeros.py
@@ -1,4 +1,4 @@
-# Award link: https://app.codility.com/cert/view/cert646E5S-D4UKRMYFRHPH26Q2/
+# Award link: https://app.codility.com/cert/view/certPGKE4Q-PRH8HW3GUWV9QRZK/
 
 def solution(A):
     def containZero(A):
@@ -15,51 +15,35 @@ def solution(A):
             return x
         return y
     def trans(n):
-        if n==0:
-            return [0,0,1]
         r2, r5=0,0
-        while n%5==0:
+        while n!=0 and n%5==0:
             r5+=1
             n=n//5
-        while n%2==0:
+        while n!=0 and n%2==0:
             r2+=1
             n=n//2
-        return [r2, r5, 0]
+        return [r2, r5]
     def trans2(n):
         if n==0:
             return [float('inf'), float('inf')]
         r2, r5=0,0
-        while n%5==0:
+        while n!=0 and n%5==0:
             r5+=1
             n=n//5
-        while n%2==0:
+        while n!=0 and n%2==0:
             r2+=1
             n=n//2
         return [r2, r5]
     l=len(A)
-    if containZero(A):
+    contZero=containZero(A)
+    if contZero:
         for i in range(l):
             for j in range(l):
                 A[i][j]=trans2(A[i][j])
-        dp=[[0]*l for _ in range(l)]
-        tmp=[0,0]
+    else:
         for i in range(l):
-            tmp=vecPlus(tmp, A[0][i])
-            dp[0][i]=tmp
-        tmp=[0,0]
-        for i in range(l):
-            tmp=vecPlus(tmp, A[i][0])
-            dp[i][0]=tmp
-        for i in range(1,l):
-            for j in range(1,l):
-                dp[i][j]=small(vecPlus(dp[i-1][j], A[i][j]), vecPlus(dp[i][j-1], A[i][j]))
-        if min(dp[-1][-1][0], dp[-1][-1][1])==0:
-            return 0
-        return 1
-    # if A does't contain 0
-    for i in range(l):
-        for j in range(l):
-            A[i][j]=trans(A[i][j])
+            for j in range(l):
+                A[i][j]=trans(A[i][j])
     dp=[[0]*l for _ in range(l)]
     tmp=[0,0]
     for i in range(l):
@@ -72,4 +56,8 @@ def solution(A):
     for i in range(1,l):
         for j in range(1,l):
             dp[i][j]=small(vecPlus(dp[i-1][j], A[i][j]), vecPlus(dp[i][j-1], A[i][j]))
+    if contZero:
+        if min(dp[-1][-1][0], dp[-1][-1][1])==0:
+            return 0
+        return 1
     return min(dp[-1][-1][0], dp[-1][-1][1])
