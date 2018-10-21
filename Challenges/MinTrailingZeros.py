f1ed726c11c4f9b9039a4c8f715974142e3c8f3f
yotti5160 <40465007+yotti5160@users.noreply.github.com>
2018-10-21 19:05:05 +0800

    Update MinTrailingZeros.py

--- Challenges/MinTrailingZeros.py
+++ Challenges/MinTrailingZeros.py
@@ -1,46 +1,35 @@
-# Award link: https://app.codility.com/cert/view/certYFT8QV-9XABHHN9Z92MSM2Y/
+# Award link: https://app.codility.com/cert/view/cert5J2CHJ-226ENHJM9F9DSQWM/
 
 def solution(A):
-    def containZero(A):
-        for i in range(l):
-            for j in range(l):
-                if A[i][j]==0:
-                    return True
-        return False
     def vecPlus(x, y):
         return [x[0]+y[0], x[1]+y[1]]
-    def small(x, y):
-        if min(x[0], x[1])<=min(y[0], y[1]):
-            return x
-        return y
     def trans(n):
         if n==0:
             return [float('inf'), float('inf')]
         r2, r5=0,0
+        while n&1==0:
+            r2+=1
+            n=n>>1
         while n%5==0:
             r5+=1
             n=n//5
-        while n%2==0:
-            r2+=1
-            n=n//2
         return [r2, r5]
     l=len(A)
-    contZero=containZero(A)
+    contZero=False
     for i in range(l):
         for j in range(l):
+            if A[i][j]==0:
+                contZero=True
             A[i][j]=trans(A[i][j])
     dp=[[0]*l for _ in range(l)]
-    tmp=[0,0]
+    tmp1, tmp2=[0,0], [0,0]
     for i in range(l):
-        tmp=vecPlus(tmp, A[0][i])
-        dp[0][i]=tmp
-    tmp=[0,0]
-    for i in range(l):
-        tmp=vecPlus(tmp, A[i][0])
-        dp[i][0]=tmp
+        tmp1, tmp2=vecPlus(tmp1, A[0][i]), vecPlus(tmp2, A[i][0])
+        dp[0][i], dp[i][0]=tmp1, tmp2
     for i in range(1,l):
         for j in range(1,l):
-            dp[i][j]=small(vecPlus(dp[i-1][j], A[i][j]), vecPlus(dp[i][j-1], A[i][j]))
+            vec1, vec2=vecPlus(dp[i-1][j], A[i][j]), vecPlus(dp[i][j-1], A[i][j])
+            dp[i][j]=[min(vec1[0], vec2[0]), min(vec1[1], vec2[1])]
     if contZero:
         return min(1, dp[-1][-1][0], dp[-1][-1][1])
     return min(dp[-1][-1][0], dp[-1][-1][1])
